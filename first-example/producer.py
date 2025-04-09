6fb0dd5820737f3082d3473f87e95624473bbc23
ivanzy <idimitryz@gmail.com>
2025-04-09 12:49:31 +0200

    fix: not show IP

--- first-example/producer.py
+++ first-example/producer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=["137.204.143.89:9094"])
+producer = KafkaProducer(bootstrap_servers=["localhost:9094"])
 
 producer.send("topic_name", value="Hello, World!".encode("utf-8"))
 producer.flush()
